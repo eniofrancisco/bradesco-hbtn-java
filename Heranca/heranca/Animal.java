ceb5c09c72b9047d6dd13f76098238d0d2489030
Enio <enio.francisco@gmail.com>
2025-07-25 22:51:34 +0000

    My Heranca

---
+++ Heranca/heranca/Animal.java
@@ -0,0 +1,10 @@
+// Classe Base
+public class Animal {
+    public void emitirSom() {
+        System.out.println("O animal faz um som.");
+    }
+
+    public void mover() {
+        System.out.println("O animal esta se movendo.");
+    }
+}
