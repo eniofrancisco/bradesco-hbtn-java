f1dfb0dcddce411fb84eb44aeaef02a5a29acfe4
Enio <enio.francisco@gmail.com>
2025-07-25 23:37:42 +0000

    forma

---
+++ Heranca/forma/FormaGeometrica.java
@@ -0,0 +1,7 @@
+public class FormaGeometrica {
+
+    // Metodo que deve ser sobrescrito pelas subclasses
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
