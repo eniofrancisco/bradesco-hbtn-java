f9d8d821650e4f37021689505c5f676f5198cdd8
Enio <enio.francisco@gmail.com>
2025-07-25 21:10:07 +0000

    My second PersonagemGame

---
+++ PersonagemGame.java
@@ -0,0 +1,24 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    // Getter para saudeAtual
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    // Setter para saudeAtual
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    // Getter para nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter para nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
