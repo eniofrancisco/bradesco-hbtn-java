2cc07e77889587dbc625748642684ad6008b4075
eniofrancisco <enio.francisco@gmail.com>
2025-08-05 02:12:20 +0000

    workflow

---
+++ CanalNotificacao.java
@@ -0,0 +1,6 @@
+
+// Interface CanalNotificacao
+public interface CanalNotificacao {
+    void notificar(Mensagem mensagem);
+}
+
---
+++ Sms.java
@@ -0,0 +1,9 @@
+
+// Implementacao: Sms
+public class Sms implements CanalNotificacao {
+    @Override
+    public void notificar(Mensagem mensagem) {
+        System.out.println("[SMS] {" + mensagem.getTipoMensagem() + "} - " + mensagem.getTexto());
+    }
+}
+
