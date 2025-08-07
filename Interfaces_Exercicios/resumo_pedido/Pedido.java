50e128fc2ab2880fdf8de57b96813dedd8cfc65a
eniofrancisco <enio.francisco@gmail.com>
2025-08-07 00:08:29 +0000

    frete

---
+++ Interfaces_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,34 @@
+
+public class Pedido {
+    private int codigo;
+    private double peso; // em gramas
+    private double total;
+    private provedores.Frete frete;
+
+    public Pedido(int codigo, double peso, double total) {
+        this.codigo = codigo;
+        this.peso = peso;
+        this.total = total;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public provedores.Frete getFrete() {
+        return frete;
+    }
+
+    public void setFrete(provedores.Frete frete) {
+        this.frete = frete;
+    }
+}
+
