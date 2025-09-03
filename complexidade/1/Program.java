61d6ea35124e1af1d0a321f1220690d5b4686b75
eniofrancisco <enio.francisco@gmail.com>
2025-09-03 03:07:14 +0000

    array

--- complexidade/1/Program.java
+++ complexidade/1/Program.java
@@ -1,7 +1,8 @@
 public class Program {
     public static void main(String[] args) {
         long inicio = System.nanoTime();
-        try {
+        
+		try {
             PessoasArray p = new PessoasArray();
             p.buscaLinear("Otavio");
             p.buscaLinear("Isabela");
