1a33c6f6d5ccd4f5b980577cd023eef872dcbc85
eniofrancisco <enio.francisco@gmail.com>
2025-09-03 02:37:34 +0000

    array

---
+++ complexidade/0/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    public static void main(String[] args) {
+        PessoasArray p = new PessoasArray();
+        p.pesquisaTempoConstante(2);   // Carlos
+        p.pesquisaTempoConstante(7);   // Hingrid
+        p.pesquisaTempoConstante(25);  // Gera exceção
+    }
+}
