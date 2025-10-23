d2287c8d1f5a8ee14958dc2ceb16dadfac5df1d2
eniofrancisco <enio.francisco@gmail.com>
2025-10-23 02:07:25 +0000

    idade_por_cargo

---
+++ 0x19/idade_por_cargo/Pessoa.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        String salarioFormatado = String.format(Locale.US, "%.6f", salario).replace('.', ',');
+        return String.format("[%d] %s %s %d R$ %s", codigo, nome, cargo, idade, salarioFormatado);
+    }
+}
