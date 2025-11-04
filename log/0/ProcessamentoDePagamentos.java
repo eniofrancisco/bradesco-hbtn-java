0c5a4f58aba9f196e78d2f395d9487b635d41514
eniofrancisco <enio.francisco@gmail.com>
2025-11-04 02:57:07 +0000

    log

---
+++ log/0/ProcessamentoDePagamentos.java
@@ -0,0 +1,32 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class ProcessamentoDePagamentos {
+
+    // Logger SLF4j
+    private static final Logger logger = LoggerFactory.getLogger(ProcessamentoDePagamentos.class);
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            logger.info("Iniciando o processamento do pagamento {}", i);
+
+            switch (i) {
+                case 1:
+                case 4:
+                    logger.error("Erro ao processar o pagamento {}: Falha na transação.", i);
+                    break;
+                case 5:
+                    logger.warn("Pagamento {} está pendente. Aguardando confirmação.", i);
+                    break;
+                default:
+                    logger.info("Pagamento {} processado com sucesso.", i);
+                    break;
+            }
+        }
+
+        logger.info("Processamento de pagamentos concluído.");
+    }
+}
